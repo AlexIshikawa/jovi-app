b78cd906b5e7e15090ec4dd7dc18d184492560de
Alexandre Ishikawa <alexandre.ishikawa@sunoresearch.com.br>
2026-09-20 20:53:29 -0300

    feat: Detalhes modo viagem

--- src/components/app-button.js
+++ src/components/app-button.js
@@ -3,25 +3,27 @@ class AppButton extends HTMLElement {
         this.classList.add("block");
 
         const href = this.getAttribute("href") || "#";
+        const variant = this.getAttribute("variant") || "primary";
+
+        const variants = {
+            primary: "bg-gradient-to-r from-brand-blue to-brand-purple text-black py-3 px-6",
+            secondary: "bg-neutral-800 text-white border border-neutral-600 py-2 px-4",
+        };
 
         const link = document.createElement("a");
-
         link.href = href;
 
         link.className = `
             flex
             items-center
             justify-center
+            gap-2
+            whitespace-nowrap
             w-full
-            py-3
-            px-6
             rounded-full
-            bg-gradient-to-r
-            from-brand-blue
-            to-brand-purple
-            text-black
             text-center
             font-medium
+            ${variants[variant]}
         `;
 
         while (this.firstChild) {
