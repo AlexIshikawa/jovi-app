fd91d2d6629383cdadbe7a4bf3cbef70c531971b
Alexandre Ishikawa <alexandre.ishikawa@sunoresearch.com.br>
2026-09-20 17:02:43 -0300

    feat: Página de Configuração e ajuste de nomenclatura

--- src/components/gradient-text.js
+++ src/components/gradient-text.js
@@ -1,12 +1,15 @@
 class AppGradientText extends HTMLElement {
-    connectedCallback() {
-        this.className = `
-        inline-block
-        bg-gradient-to-r from-brand-blue to-brand-purple
-        bg-clip-text text-transparent
-        font-bold
-      `;
-    }
+  connectedCallback() {
+    this.classList.add(
+      "inline-block",
+      "bg-gradient-to-r",
+      "from-brand-blue",
+      "to-brand-purple",
+      "bg-clip-text",
+      "text-transparent",
+      "font-bold"
+    );
+  }
 }
 
 customElements.define('app-gradient-text', AppGradientText);
